9c047da608484d12f949ab631ccc1533f18fa9f9
lixuebin <lixuebin@cdeledu.com>
2023-04-06 10:48:28 +0800

    add err constants

--- def/customError.go
+++ def/customError.go
@@ -21,20 +21,61 @@ func NewCustomError(et ErrorType, code int, msg string, context any) CustomError
 	}
 }
 
+const (
+	// call out err
+	CALL_OPTION_C = -11
+	CALL_OPTION_M = "call_option should not be null!"
+
+	CALL_PARA_C = -12
+	CALL_PARA_M = "call out para can't convert to json!"
+
+	CALL_SIGN_C = -13
+	CALL_SIGN_M = "can't sign for the call: "
+
+	CALL_ENCRYPT_C = -14
+	CALL_ENCRYPT_M = "can't encrypt for the call: "
+
+	// call returned err
+	RESPONSE_SIGN_C = -21
+	RESPONSE_SIGN_M = "response error, can't verify the sign: "
+
+	RESPONSE_DECRYPT_C = -22
+	RESPONSE_DECRYPT_M = "response error, can't decrypt input: "
+
+	RESPONSE_BODY_EMPTY_C = -31
+	RESPONSE_BODY_EMPTY_M = "response error, empty body!"
+
+	RESPONSE_ERROR_C = -32
+	RESPONSE_ERROR_M = "returned error: "
+
+	// common error
+	VERIFY_C = -41
+	VERIFY_M = "verify failed: "
+
+	ENV_C = -51
+	ENV_M = "env error: "
+
+	SYS_C = -61
+	SYS_M = "sys error: "
+
+	UNKNOWN_C = -99
+	UNKNOWN_M = "undefined error: "
+)
+
 // call out err
-var E_CALL_OPTION = ErrorDefine{-11, "call_option should not be null!"}
-var E_CALL_PARA = ErrorDefine{-12, "call out para can't convert to json!"}
-var E_CALL_SIGN = ErrorDefine{-13, "can't sign for the call: "}
-var E_CALL_ENCRYPT = ErrorDefine{-14, "can't encrypt for the call: "}
+var E_CALL_OPTION = ErrorDefine{CALL_OPTION_C, CALL_OPTION_M}
+var E_CALL_PARA = ErrorDefine{CALL_PARA_C, CALL_PARA_M}
+var E_CALL_SIGN = ErrorDefine{CALL_SIGN_C, CALL_SIGN_M}
+var E_CALL_ENCRYPT = ErrorDefine{CALL_ENCRYPT_C, CALL_ENCRYPT_M}
 
 // call returned err
-var E_RESPONSE_SIGN = ErrorDefine{-21, "response error, can't verify the sign: "}
-var E_RESPONSE_DECRYPT = ErrorDefine{-22, "response error, can't decrypt input: "}
-var E_RESPONSE_BODY_EMPTY = ErrorDefine{-31, "response error, empty body!"}
-var E_RESPONSE_ERROR = ErrorDefine{-32, "returned error: "}
+var E_RESPONSE_SIGN = ErrorDefine{RESPONSE_SIGN_C, RESPONSE_SIGN_M}
+var E_RESPONSE_DECRYPT = ErrorDefine{RESPONSE_DECRYPT_C, RESPONSE_DECRYPT_M}
+var E_RESPONSE_BODY_EMPTY = ErrorDefine{RESPONSE_BODY_EMPTY_C, RESPONSE_BODY_EMPTY_M}
+var E_RESPONSE_ERROR = ErrorDefine{RESPONSE_ERROR_C, RESPONSE_ERROR_M}
 
 // common error
-var E_VERIFY = ErrorDefine{-41, "verify failed: "}
-var E_ENV = ErrorDefine{-51, "env error: "}
-var E_SYS = ErrorDefine{-61, "sys error: "}
-var E_UNKNOWN = ErrorDefine{-99, "undefined error: "}
+var E_VERIFY = ErrorDefine{VERIFY_C, VERIFY_M}
+var E_ENV = ErrorDefine{ENV_C, ENV_M}
+var E_SYS = ErrorDefine{SYS_C, SYS_M}
+var E_UNKNOWN = ErrorDefine{UNKNOWN_C, UNKNOWN_M}
