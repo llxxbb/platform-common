18eff163adc702f60b81114d8b5afbef384dc8c2
lixuebin <lixuebin@cdeledu.com>
2023-09-01 14:03:53 +0800

    update ParaIn and ParaOut

--- access/paraIn.go
+++ access/paraIn.go
@@ -5,13 +5,13 @@ import (
 )
 
 type ParaIn[T any] struct {
-	Data     T                 `json:"data"`     // 要传送的业务数据
-	TraceId  string            `json:"traceId"`  // 用于链路分析
-	PTraceId string            `json:"pTraceId"` // 父 traceId，用于解决跟踪分叉问题
-	RefId    string            `json:"refId"`    // 关联业务的ID,用于审计
-	Label    map[string]string `json:"label"`    // 用于标记请求者的额外信息
-	Note     string            `json:"note"`     // 备注信息， 会存储于行为采集
-	Time     string            `json:"time"`     // 发起端的调用时间, Long 型的字符串
+	Data     T                 `json:"data"`               // 要传送的业务数据
+	TraceId  string            `json:"traceId,omitempty"`  // 用于链路分析
+	PTraceId string            `json:"pTraceId,omitempty"` // 父 traceId，用于解决跟踪分叉问题
+	RefId    string            `json:"refId,omitempty"`    // 关联业务的ID,用于审计
+	Label    map[string]string `json:"label,omitempty"`    // 用于标记请求者的额外信息
+	Note     string            `json:"note,omitempty"`     // 备注信息， 会存储于行为采集
+	Time     string            `json:"time,omitempty"`     // 发起端的调用时间, Long 型的字符串
 }
 
 func CreateFrom[T any](data T) ParaIn[T] {
