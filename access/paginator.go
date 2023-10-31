9706cb38ba49026c2bb30dd571e39197916f18cd
lixuebin <lixuebin@cdeledu.com>
2023-10-31 17:56:45 +0800

    fix: type error

--- access/paginator.go
+++ access/paginator.go
@@ -1,6 +1,6 @@
 package access
 
 type Paginator[T any] struct {
-	FromId T `json:"fromId"` // 分页的起始ID（不含）
-	Limit  T `json:"limit"`  // 限制返回的结果数
+	FromId T      `json:"fromId"` // 分页的起始ID（不含）
+	Limit  uint16 `json:"limit"`  // 限制返回的结果数
 }
