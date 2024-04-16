d4dabd7c84f9033d80790962e6993fbb82f2c2eb
lixuebin <lixuebin@cdeledu.com>
2024-04-16 10:48:13 +0800

    extend tag ability: to int value for map

--- tool/tag/tag2mapN.go
+++ tool/tag/tag2mapN.go
@@ -5,13 +5,12 @@ import (
 	"gitlab.cdel.local/platform/go/platform-common/def"
 	"go.uber.org/zap"
 	"reflect"
-	"strconv"
-	"strings"
 )
 
-var mapTag = "map"
+var mapNTag = "mapN"
 
-func ToMap(obj any) (map[string]string, *def.CustomError) {
+// ToMapN 用于处理数值标签
+func ToMapN(obj any) (map[string]int, *def.CustomError) {
 	if obj == nil {
 		return nil, nil
 	}
@@ -23,10 +22,10 @@ func ToMap(obj any) (map[string]string, *def.CustomError) {
 	}
 
 	cnt := t.NumField()
-	var data = make(map[string]string, cnt)
+	var rtn = make(map[string]int, cnt)
 	for i := 0; i < cnt; i++ {
 		field := t.Field(i)
-		tag := field.Tag.Get(mapTag)
+		tag := field.Tag.Get(mapNTag)
 		if tag == "" {
 			continue
 		}
@@ -34,47 +33,23 @@ func ToMap(obj any) (map[string]string, *def.CustomError) {
 		fV := v.Field(i)
 		fT := fV.Type().Name()
 		switch fT {
-		case "string":
-			rValue := fV.Interface().(string)
-			if omitEmpty && rValue == "" {
-				continue
-			}
-			data[key] = rValue
-		case "bool":
-			rValue := fV.Bool()
-			if omitEmpty && !rValue {
-				continue
-			}
-			data[key] = strconv.FormatBool(rValue)
 		case "int":
 			rValue := int(fV.Int())
 			if omitEmpty && rValue == 0 {
 				continue
 			}
-			data[key] = strconv.Itoa(rValue)
+			rtn[key] = rValue
 		default:
 			msg := fmt.Sprintf("%s unsupported type: %s", def.SYS_M, fT)
 			zap.L().Error(msg)
 			return nil, def.NewCustomError(def.ET_SYS, def.SYS_C, msg, nil)
 		}
 	}
-	return data, nil
+	return rtn, nil
 }
 
-// return key and omitEmpty
-func isOmitEmpty(tag string) (string, bool) {
-	split := strings.Split(tag, ",")
-	omitEmpty := false
-	if len(split) > 1 {
-		if split[1] == "omitempty" {
-			omitEmpty = true
-		}
-	}
-	return split[0], omitEmpty
-}
-
-// FromMap notice: para `remove` will remove from `mVal` after restored
-func FromMap(mVal map[string]string, ins any, remove bool) *def.CustomError {
+// FromMapN notice: para `remove` will remove from `mVal` after restored
+func FromMapN(mVal map[string]int, ins any, remove bool) *def.CustomError {
 	// check before process
 	if mVal == nil || len(mVal) == 0 {
 		return nil
@@ -85,7 +60,7 @@ func FromMap(mVal map[string]string, ins any, remove bool) *def.CustomError {
 
 	for i := 0; i < v.NumField(); i++ {
 		field := t.Field(i)
-		tag := field.Tag.Get(mapTag)
+		tag := field.Tag.Get(mapNTag)
 		if tag == "" {
 			continue
 		}
@@ -97,13 +72,8 @@ func FromMap(mVal map[string]string, ins any, remove bool) *def.CustomError {
 		fV := v.Field(i)
 		fT := fV.Type().Name()
 		switch fT {
-		case "string":
-			fV.SetString(mV)
-		case "bool":
-			parseBool, _ := strconv.ParseBool(mV)
-			fV.SetBool(parseBool)
 		case "int":
-			parseInt, _ := strconv.Atoi(mV)
+			parseInt := mV
 			fV.SetInt(int64(parseInt))
 		default:
 			msg := fmt.Sprintf("%s unsupported type: %s", def.SYS_M, fT)
--- tool/tag/tag2map_test.go
+++ tool/tag/tag2map_test.go
@@ -8,33 +8,43 @@ import (
 
 type User struct {
 	Name  string `json:"name" map:"name"`
-	Age   int    `json:"age" map:"age"`
+	Age   int    `json:"age" mapN:"age"`
 	Male  bool   `json:"male" map:"male"`
 	Empty int    `json:"empty" map:"empty,omitempty"`
-	Value int    `json:"value" map:"value,omitempty"`
+	Value int    `json:"value" mapN:"value,omitempty"`
 	Sub
 }
 type Sub struct {
 	Hi string
 }
 
-func Test_HashSha(t *testing.T) {
-	//to map
+func Test_tag2map(t *testing.T) {
+	// to map -------------------------------
 	user := User{Name: "John Doe", Age: 30, Male: true, Value: 3}
 	rMap, _ := ToMap(user)
 	rString := fmt.Sprint(rMap)
-	assert.Equal(t, "map[age:30 male:true name:John Doe value:3]", rString)
+	assert.Equal(t, "map[male:true name:John Doe]", rString)
 	// pointer
-	assert.Equal(t, "map[age:30 male:true name:John Doe value:3] <nil>", fmt.Sprint(ToMap(&user)))
+	assert.Equal(t, "map[male:true name:John Doe] <nil>", fmt.Sprint(ToMap(&user)))
+	// to map Num---------------------------------------
+	rMapN, _ := ToMapN(user)
+	rStringN := fmt.Sprint(rMapN)
+	assert.Equal(t, "map[age:30 value:3]", rStringN)
+	// pointer
+	assert.Equal(t, "map[age:30 value:3] <nil>", fmt.Sprint(ToMapN(&user)))
 
 	// from map
 	another := User{}
 	_ = FromMap(rMap, &another, false)
+	assert.Equal(t, 2, len(rMap))
+	_ = FromMapN(rMapN, &another, false)
+	assert.Equal(t, 2, len(rMap))
 	assert.Equal(t, user, another)
-	assert.Equal(t, 4, len(rMap))
 
 	another = User{}
 	_ = FromMap(rMap, &another, true)
-	assert.Equal(t, user, another)
 	assert.Equal(t, 0, len(rMap))
+	_ = FromMapN(rMapN, &another, true)
+	assert.Equal(t, 0, len(rMap))
+	assert.Equal(t, user, another)
 }
