d2fa56886fb09f1d0d53282f2556e1d1c9612459
wangyifeng <dl@dldeiMac.local>
2024-05-29 18:10:27 +0800

    增加参数

--- access/paginator.go
+++ access/paginator.go
@@ -1,6 +1,7 @@
 package access
 
 type Paginator[T any] struct {
-	FromId T      `json:"fromId"` // 分页的起始ID（不含）
-	Limit  uint16 `json:"limit"`  // 限制返回的结果数
+	FromId T      `json:"fromId"`         // 分页的起始ID（不含）
+	Limit  uint16 `json:"limit"`          // 限制返回的结果数
+	Desc   bool   `json:"desc,omitempty"` // 是否倒序
 }
